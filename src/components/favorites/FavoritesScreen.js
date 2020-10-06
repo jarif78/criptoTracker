c631f904db397776bac0369a78be634da1164454
ARiel Fernández <jarif78@gmail.com>
2020-10-06 12:26:53 -0300

    Implementacion de favorites

--- src/components/favorites/FavoritesScreen.js
+++ src/components/favorites/FavoritesScreen.js
@@ -1,22 +1,58 @@
 import React, {Component} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, FlatList, StyleSheet} from 'react-native';
+import CoinsItem from 'cryptoTracker/src/components/coins/CoinsItem';
 import FavoritesEmptyState from './FavoritesEmptyState';
 import Colors from '../../res/colors';
 import Storage from '../../libs/storage';
 
 class FavoritesScreen extends Component {
+  state = {
+    favorites: [],
+  };
+
   getFavorites = async () => {
     try {
-      const allKeys = Storage.instance.getAllkeys();
-      console.log('allkeys', allKeys);
+      const allKeys = await Storage.instance.getAllkeys();
+      const keys = allKeys.filter((key) => key.includes('favorite-'));
+      console.log('keys', keys);
+
+      const favs = await Storage.instance.multiGet(keys);
+      const favorites = favs.map((fav) => JSON.parse(fav[1]));
+      console.log('favorites', favorites);
+      this.setState({favorites});
     } catch (error) {
       console.log('get favorites error', error);
     }
   };
+
+  handlePress = (coin) => {
+    this.props.navigation.navigate('CoinDetail', {coin});
+  };
+
+  componentDidMount() {
+    //this.getFavorites();
+    this.props.navigation.addListener('focus', this.getFavorites);
+  }
+
+  componentWillUnmount() {
+    // Si no se quita queda como un componente sombie
+    this.props.navigation.removeListener('focus', this.getFavorites);
+  }
+
   render() {
+    const {favorites} = this.state;
     return (
       <View style={styles.container}>
-        <FavoritesEmptyState />
+        {favorites.length === 0 ? <FavoritesEmptyState /> : null}
+
+        {favorites.length > 0 ? (
+          <FlatList
+            data={favorites}
+            renderItem={({item}) => (
+              <CoinsItem item={item} onPress={() => this.handlePress(item)} />
+            )}
+          />
+        ) : null}
       </View>
     );
   }
